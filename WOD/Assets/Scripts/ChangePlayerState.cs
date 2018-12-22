569f72541a6db018df6a84cc5ff1242d89be6ddb
이예준 <blackballpen02@gmail.com>
2018-12-22 18:52:41 +0900

    scene added

---
+++ WOD/Assets/Scripts/ChangePlayerState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayerState : MonoBehaviour
+{
+    [SerializeField]
+    Animator ani;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void changeState(int state)
+    {
+        ani.SetInteger("PlayerState", 1);
+    }
+}
---
+++ WOD/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Section
+{
+    naturalSciences,
+    liberalArts
+};
+
+public class PlayerManager
+{
+    int talent;
+    int health;
+    int[] knowledge;
+
+    private static PlayerManager instance = null;
+    private PlayerManager()
+    {
+        talent = 0;
+        health = 0;
+        knowledge = new int[2];
+        knowledge[(int)Section.naturalSciences] = 0;
+        knowledge[(int)Section.liberalArts] = 0;
+    }
+
+    public static PlayerManager getInstance()
+    {
+        if (instance == null)
+            instance = new PlayerManager();
+        return instance;
+    }
+
+    public int Talent
+    {
+        get { return talent; }
+        set { talent = value; }
+    }
+
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public void setKnowledge(int index,int val)
+    {
+        knowledge[index] = val;
+    }
+
+    public int getKnowledge(int index)
+    {
+        return knowledge[index];
+    }
+}
